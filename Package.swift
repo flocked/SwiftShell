c68eaf5b9cc707d152a579b6afe746a72d112afa
Florian Zand <florianzand@me.com>
2024-01-18 09:39:11 +0100

    Initial

--- Package.swift
+++ Package.swift
@@ -1,16 +1,21 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
 	name: "SwiftShell",
+    platforms: [.macOS("10.15.1"), .iOS(.v14), .macCatalyst(.v14), .tvOS(.v14), .watchOS(.v6)],
 	products: [
 		.library(
 			name: "SwiftShell",
 			targets: ["SwiftShell"])
 	],
+    dependencies: [
+        .package(url: "https://github.com/flocked/FZSwiftUtils.git", branch: "main"),
+    ],
 	targets: [
 		.target(
 			name: "SwiftShell",
+            dependencies: ["FZSwiftUtils"],
 			swiftSettings: [
 				.define("DEBUG", .when(configuration: .debug)),
 			]),
